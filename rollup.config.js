14fce8b8a22d0b40a02115286cd418df21beb0e4
Colin Cai <therealcreative0708@gmail.com>
2024-11-08 11:22:05 -0500

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import html from "@rollup/plugin-html";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+
+/** { @type import("rollup").InputOptions } */
+export default {
+  input: "src/main.js",
+  output: {
+    file: "dist/bundle.js",
+  },
+  plugins: [html(), livereload(), serve("dist")],
+};
